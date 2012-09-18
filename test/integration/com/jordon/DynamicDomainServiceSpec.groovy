e31aa96cc0314d25f6265077832d0a8d8ece9107
jsaardchit <jsaardchit@spireon.com>
2012-09-18 13:39:13 -0700

    Adding tests for dynamic domain class generation

---
+++ test/integration/com/jordon/DynamicDomainServiceSpec.groovy
@@ -0,0 +1,39 @@
+package com.jordon
+
+import grails.plugin.spock.IntegrationSpec;
+
+import org.grails.dynamicdomain.DynamicDomainService;
+
+class DynamicDomainServiceSpec extends IntegrationSpec {
+	def grailsApplication
+	def dynamicDomainService
+	def domainClass = '''public class DDSTest {
+		String name
+		String value
+		static constraints = {
+			name( nullable:false )
+			value( nullable:false )
+		}
+}
+'''
+	def "test dynamic domain class creation"() {
+		setup:
+		dynamicDomainService.registerDomainClass( domainClass )
+		
+		when:
+		def dc = grailsApplication.getClassForName( 'DDSTest' ).newInstance()
+		dc.name = name
+		dc.value = value
+		dc.validate()
+		
+		then:
+		dc.hasErrors() != valid
+		
+		where:
+		name			| value			| valid
+		'TestName'		| 'TestValue'	| true
+		null			| null			| false
+		'TestName'		| null			| false
+		null			| 'TestValue'	| false
+	}
+}
